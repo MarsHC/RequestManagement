55b264d519df6d456306aea61f035c49bb667029
黄超 <huangchao@sparkingcd.com>
2020-11-19 16:33:24 +0800

    初始化项目

---
+++ RequestMgmt/RequestMgmt/Classes/RM/Controller/RMHistoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RMHistoryViewController.swift
+//  RequestMgmt
+//
+//  Created by 黄超 on 2020/11/19.
+//  Copyright © 2020 黄超. All rights reserved.
+//
+
+import UIKit
+
+class RMHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ RequestMgmt/RequestMgmt/Classes/RM/Controller/RMResultViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RMResultViewController.swift
+//  RequestMgmt
+//
+//  Created by 黄超 on 2020/11/19.
+//  Copyright © 2020 黄超. All rights reserved.
+//
+
+import UIKit
+
+class RMResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red;
+        
+        navigationItem.title = "Request Result"
+    }
+    
+
+}
